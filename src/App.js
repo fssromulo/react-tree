ad532bb52eb2a15db24780a5e991ffdf5d51541c
Rômulo Fernando <fssromulo@gmail.com>
2019-11-14 02:40:17 -0300

    Ajuste para marcar/desmarcar os itens e abrir e fechar o componente

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import React, { Component } from 'react';
 import "bootstrap/dist/css/bootstrap.css";
 
+import 'materialize-css/dist/css/materialize.min.css';
+import 'materialize-css/dist/js/materialize.min.js';
 import ReactTree from './components/react-tree/index'
 
 class App extends Component {
-
 	render() {
+		// Simula a busca de dados numa api ...
+		const arrDadosArvore = require('./dados/data_copy.json');
 
-		// console.log(this.state.objListData);
 		return (
 			<div className="App">
-				<h1> React - Arvore recursiva </h1>
-				<br />
-				<br />
-				<ReactTree
-				/>
-			</div>
+				<form action="#">
+					<h1> Árvore recursiva </h1>
+
+					<br />
+					<br />
+					<ReactTree
+						arrDadosArvore={arrDadosArvore}
+					/>
+				</form>
+			</div >
 		);
 	}
 }
